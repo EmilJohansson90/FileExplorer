9441cb63c622e27a4fcaf5446b7f9d395cf25ed8
Emil Johansson <emil.johansson@studerande-skys.se>
2021-11-24 21:42:04 +0100

    1.4

--- FileExplorer/FolderView.cs
+++ FileExplorer/FolderView.cs
@@ -9,34 +9,47 @@ namespace FileExplorer
 {
     class FolderView : ConsoleExplorer
     {
+        private string _currentFileName;
+
         public void ListInventory()
         {
             while (true)
-            {
+            {                
                 Console.Clear();
                 string[] fileArray = Directory.GetFileSystemEntries(".");
 
-                for (int i = 0; i < fileArray.Length; i++)
+                if (_viewState == ViewState.List)
                 {
-                    if (_indexNumber == i)
+                    for (int i = 0; i < fileArray.Length; i++)
                     {
-                        Console.BackgroundColor = ConsoleColor.DarkGray;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                    }
-                    else
-                    {
-                        Console.BackgroundColor = ConsoleColor.Black;
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
+                        if (_indexNumber == i)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
 
-                    if (File.Exists(fileArray[i]))
-                    {
-                        Console.WriteLine($"- {Path.GetFileName(fileArray[i])}");
-                    }
-                    else if (Directory.Exists(fileArray[i]))
-                    {
-                        Console.WriteLine($"# {Path.GetFileName(fileArray[i])}");
-                    }
+                        if (File.Exists(fileArray[i]))
+                        {
+                            Console.WriteLine($"- {Path.GetFileName(fileArray[i])}");
+                        }
+                        else if (Directory.Exists(fileArray[i]))
+                        {
+                            Console.WriteLine($"# {Path.GetFileName(fileArray[i])}");
+                        }
+
+                        _currentFileName = Path.GetFileName(fileArray[i]);
+                    }                    
+                } else
+                {
+                    Console.WriteLine(File.ReadAllText($"{_currentFileName}"));
+                    Console.WriteLine("\n\nTryck på valfri tangent för att gå tillbaka till listan");
+                    Console.ReadKey();
+                    _viewState = ViewState.List;
                 }
 
                 ConsoleKey input = Console.ReadKey().Key;
@@ -47,17 +60,19 @@ namespace FileExplorer
                 else if (input == ConsoleKey.UpArrow && _indexNumber > 0)
                 {
                     Up();
+                } else if(input == ConsoleKey.Spacebar)
+                {
+                    _viewState = ViewState.FileView;
                 }
             }
         }
 
-        public void Up()
+        public string CurrentFileName
         {
-            _indexNumber--;
-        }
-        public void Down()
-        {
-            _indexNumber++;
+            get
+            {
+                return _currentFileName;
+            }
         }
     }
 }
---
+++ FileExplorer/ViewState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExplorer
+{
+    enum ViewState
+    {
+        List,
+        FileView
+    }
+}
